1a1c148b14c992d8825f6bb398d53ff265897b79
hsgyeong <sgyeong0711@gmail.com>
2024-08-24 23:57:51 +0900

    웹개발환경 설정

---
+++ Blog/Hello/src/HelloWorld.java
@@ -0,0 +1,15 @@
+// 이클립스 = Ctrl+F11
+// KeyMap = IntelliJ = shift+F10
+public class HelloWorld {
+
+	//스프링부트 연습을 위한 테스트
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+
+}
