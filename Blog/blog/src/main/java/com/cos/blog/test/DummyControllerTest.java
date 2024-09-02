0ac5db7d6a1bb41b6078427af9fc67fbf18e0fbb
hsgyeong <sgyeong0711@gmail.com>
2024-09-02 23:35:09 +0900

    select, update

--- Blog/blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ Blog/blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,16 +1,25 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
 
 // html 파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
@@ -19,6 +28,45 @@ public class DummyControllerTest {
 	@Autowired		// 의존성 주입 (DI)
 	private UserRepository userRepository;
 	
+	// save 함수는 id를 전달하지 않으면 insert
+	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 있을때 update
+	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 없을때 insert
+	// email, password
+	
+	@Transactional
+	@PutMapping("/dummy/user/{id}")					// @RequestBody JSON 데이터를 받기 위해 사용하는 어노테이션
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // JSON 데이터를 요청 -> Java Object MessageConverter의 Jackson 라이브러리가 변환해서 받아줌
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		// userRepository.save(user);
+		
+		// 더티 체킹
+		return null;
+	}
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	// org.springframework.data.domain.Page
+	// 한 페이지당 2건의 데이터를 리턴받을 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
 	// {id} 주소로 파라미터를 전달 받을 수 있음
 	// http://localhost:8000/blog/dummy/user/3
 	@GetMapping("/dummy/user/{id}")
