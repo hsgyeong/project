08013f2b4f071938021540d88f74da6dd5dbd226
hsgyeong <sgyeong0711@gmail.com>
2024-09-07 23:57:52 +0900

    회원가입

--- Blog/blog/src/main/resources/static/js/user.js
+++ Blog/blog/src/main/resources/static/js/user.js
@@ -18,16 +18,16 @@ let index = {
 		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
 		$.ajax({
 			type : "POST",
-			url : "/api/user",
+			url : "/blog/api/user",
 			data : JSON.stringify(data), // http body 데이터
 			contentType : "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
 			dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (json 형태라면 javascript object로 변경)
 		}).done(function(res){
 			alert("회원가입이 완료되었습니다.");
+		console.log(res);
 			location.href="/blog";
-			//13:56
-		}).fail(function(){
-			
+		}).fail(function(error){
+			alert(JSON.stringify(error));
 		}); 
 	}
 }
