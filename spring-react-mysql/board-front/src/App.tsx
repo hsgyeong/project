c8e0c1570a6b3f7cad57cd8c04e46e43efb820e9
hsgyeong <sgyeong0711@gmail.com>
2024-10-28 23:39:27 +0900

    input box

--- spring-react-mysql/board-front/src/App.tsx
+++ spring-react-mysql/board-front/src/App.tsx
@@ -5,13 +5,12 @@ import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import commentListMock from 'mocks/comment-list.mock';
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from 'components/InputBox';
 
 function App() {
   return (
   <>
-  <div style={{ display: 'flex', flexDirection: 'column', columnGap: '30px', rowGap: '20px' }}>
-    {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
-  </div>
+    <InputBox />
   </>
   );
 }
