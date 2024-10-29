18a792d7b7b457dad27b687bace9e135df557594
hsgyeong <sgyeong0711@gmail.com>
2024-10-29 12:38:36 +0900

    input box

--- spring-react-mysql/board-front/src/App.tsx
+++ spring-react-mysql/board-front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { commentListItem, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
 import Top3Item from 'components/Top3Item';
@@ -8,9 +8,12 @@ import FavoriteItem from 'components/FavoriteItem';
 import InputBox from 'components/InputBox';
 
 function App() {
+
+  const [value, setValue] = useState<string>('');
+
   return (
   <>
-    <InputBox />
+    <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={true} setValue={setValue} message='sss'/>
   </>
   );
 }
