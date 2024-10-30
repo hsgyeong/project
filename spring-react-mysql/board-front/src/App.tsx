14228ccba2afca19143e700b10e64ec588ca414c
hsgyeong <sgyeong0711@gmail.com>
2024-10-30 12:52:19 +0900

    footer 제작중

--- spring-react-mysql/board-front/src/App.tsx
+++ spring-react-mysql/board-front/src/App.tsx
@@ -1,19 +1,11 @@
-import React, { useState } from 'react';
+import Footer from 'layouts/Footer';
 import './App.css';
-import { commentListItem, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import commentListMock from 'mocks/comment-list.mock';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
-
+// 09:30
 function App() {
-
-  const [value, setValue] = useState<string>('');
 
   return (
   <>
-    <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={true} setValue={setValue} message='sss'/>
+    <Footer />
   </>
   );
 }
